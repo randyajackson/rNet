ed0200652a741400ef042c2677857bfb13da4d5a
randyajackson <jrandy24@gmail.com>
2019-05-09 20:29:43 -0600

    selenium in script form working, storing in mdb

--- data/crypto.py
+++ data/crypto.py
@@ -1,33 +1,37 @@
+
+# from bs4 import BeautifulSoup
+# import requests
+
+# r = requests.get(r"https://www.cryptocompare.com/")
+
+# data = r.text
+# soup = BeautifulSoup(data, 'html5lib')
+
+# div = soup.div
+# crypto = div.select("div", _class="panel-body")
+
+# print(crypto)
+
+
 import time
-from bs4 import BeautifulSoup
-import requests
+from urllib.request import urlopen as uReq
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from pymongo import MongoClient
 
-r = requests.get(r"https://www.cryptocompare.com/")
+chrome_path = r"C:\Users\dell\Desktop\chromeDriver\chromedriver.exe"
+driver = webdriver.Chrome(chrome_path)
 
-data = r.text
-soup = BeautifulSoup(data, 'html5lib')
+url = r"https://www.cryptocompare.com/coins/list/USD/1"
+driver.get(url)
 
-div = soup.div
-crypto = div.select("div", _class="panel-body")
+time.sleep(15)
+table = driver.find_elements_by_class_name("table-coins")
 
-print(crypto)
+prices = []
 
+data = table[0].text.splitlines()
 
-
-# from urllib.request import urlopen as uReq
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-
-# chrome_path = r"C:\Users\dell\Desktop\chromeDriver\chromedriver.exe"
-# driver = webdriver.Chrome(chrome_path)
-# url = r"https://www.cryptocompare.com/coins/list/USD/1"
-# driver.get(url)
-
-# table = driver.find_elements_by_class_name("table-coins")
-
-# prices = []
-
-# data = table[0].text.splitlines()
-
-# for x in data:
-#     prices.append(x)
+for x in data:
+    prices.append(x)
+print(prices)
