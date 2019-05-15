35ffe678a75376548c39849fddc2ebaa0a6d5a60
Randy Jackson <42244587+randyajackson@users.noreply.github.com>
2019-05-15 12:27:23 -0600

    5/15/19 10p

--- data/crypto.py
+++ data/crypto.py
@@ -1,50 +1,53 @@
-
-# from bs4 import BeautifulSoup
-# import requests
-
-# r = requests.get(r"https://www.cryptocompare.com/")
-
-# data = r.text
-# soup = BeautifulSoup(data, 'html5lib')
-
-# div = soup.div
-# crypto = div.select("div", _class="panel-body")
-
-# print(crypto)
-
-
 import time
 from urllib.request import urlopen as uReq
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pymongo import MongoClient
+import threading
 
 chrome_path = r"C:\Users\dell\Desktop\chromeDriver\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
+
 
 url = r"https://www.cryptocompare.com/coins/list/USD/1"
 driver.get(url)
 
 time.sleep(15)
-table = driver.find_elements_by_class_name("table-coins")
 
-data = table[0].text.splitlines()
+#collect grabs the state of the data every 5 seconds
 
-client = MongoClient()
-db = client['crypto']
-prices = db.crypto_data_prices
+def collect():
+    threading.Timer(5.0, collect).start()
+    table = driver.find_elements_by_class_name("table-coins")
 
-x = 7
+    data = table[0].text.splitlines()
 
-while x < len(data):
-    entry = {}
-    entry['id#'] = data[x]
-    entry['coinName'] = data[x + 1]
-    entry['coinSName'] = data[x + 2]
-    entry['coinPrice'] = data[x + 3]
-    entry['coinTotal'] = data[x + 4]
-    entry['coin24'] = data[x + 5]
-    x += 6
-    prices.insert_one(entry)
+    client = MongoClient()
+    db = client['crypto']
+    prices = db.crypto_data_prices
+
+    x = 7
+
+    while x < len(data):
+        prices.update_one(
+            {"coinName" : data[x + 1] },
+                {"$set" :
+                    {
+                        'id#' : data[x],
+                        'coinName' : data[x + 1],
+                        'coinSName' : data[x + 2],
+                        'coinPrice' : data[x + 3],
+                        'coinTotal' : data[x + 4],
+                        'coin24' : data[x + 5]
+                    }
+                }
+        )
+        x += 6
+
+    driver.refresh()
+    time.sleep(5)
+
+collect()
+
     
 
