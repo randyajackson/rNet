e4483ba6320c4876ccada6dba30c68b07e104789
randyajackson <jrandy24@gmail.com>
2019-05-15 22:29:47 -0600

    5/15/19 10:30p

--- data/crypto_prices/client/src/App.js
+++ data/crypto_prices/client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
+function App(){
+  
+  const sayHello = () => {
+    alert("hello");
+  };
+
+  return(
+    <div> 
+      <h1>Hello React</h1>
+      <button onClick={sayHello}>Hello</button>
     </div>
   );
 }
