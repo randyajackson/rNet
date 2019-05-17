ca0dfa8fd8e64d0eb2d63d03e22a8cf71cd9fa0b
randyajackson <jrandy24@gmail.com>
2019-05-16 20:56:05 -0600

    5/16/19 9p

--- data/crypto_prices/server/server.js
+++ data/crypto_prices/server/server.js
@@ -5,6 +5,27 @@ const app = express()
 
 app.use(express.json())
 
+const db = "mongodb://localhost/crypto"
+
+mongoose.connect(db, ({useNewUrlParser: true}))
+.then(console.log("connected to mongoDB"))
+.catch (err=>console.log(err));
+
+const priceSchema = new mongoose.Schema({
+    "id#" : String,
+    coinName : String,
+    coinSName : String,
+    coinPrice : String,
+    coinTotal : String,
+    coin24 : String
+ })
+
+const cryptoPrices = mongoose.model('crypto_data_prices', priceSchema)
+
+app.get("/crypto", (req, res) =>{
+    cryptoPrices.find().then(prices => res.json(prices))
+});
+
 
 app.listen(5000, () => {
     console.log("server is running at port 5000")
