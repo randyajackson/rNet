b876462c66fa3f28664b76b3342cdfe28133f558
randyajackson <rajackson.dev@gmail.com>
2019-07-18 19:22:05 -0600

    7/18/19 7:20p

--- data/index.js
+++
@@ -1,6 +0,0 @@
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
