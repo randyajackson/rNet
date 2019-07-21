a9bb5ae56ce087864aa1e5a083e156cf2eee5f0a
randyajackson <rajackson.dev@gmail.com>
2019-07-21 10:23:49 -0600

    7/21/19 10:25a

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require ('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//connect creates a promise
+mongoose.connect('mongodb://localhost/crypto', {useNewUrlParser: true, useCreateIndex: true}) 
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect', err));
+
+const connection = mongoose.connection;
+
+//once connection is open show log
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+const cryptoRouter = require('./routes/crypto');
+
+
+app.use('/crypto', cryptoRouter);
+
+
+app.listen(port, () => {
+    console.log('Server is running on port:', {port});
+});
