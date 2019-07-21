4ab8b4369a9aaf7eb26b1139dc7337f8572538e3
randyajackson <rajackson.dev@gmail.com>
2019-07-21 12:35:25 -0600

    7/21/19 12:35p

--- backend/models/crypto.model.js
+++ backend/models/crypto.model.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-
+ 
 const cryptoSchema = new Schema({
-   "id#" : Number,
-   "coinName" : String,
-   "coinSName" : String,
-   "coinPrice" : Number,
-   "coinTotal" : String,
-   "coin24" : Number
+   id_number : Number,
+   coinName : String,
+   coinSName : String,
+   coinPrice : String,
+   coinTotal : String,
+   coin24 : String
 
-});
+},
+{ collection: 'crypto_data_prices'}
+);
 
 const Crypto = mongoose.model('Crypto', cryptoSchema);
 
