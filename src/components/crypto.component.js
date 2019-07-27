5221b7bc096b8499c83cda7100c4a356858d05c0
randyajackson <rajackson.dev@gmail.com>
2019-07-27 10:46:19 -0600

    7/27/19 10:45a thank god

--- src/components/crypto.component.js
+++ src/components/crypto.component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const Price = props => (
     <tr>
@@ -19,7 +20,19 @@ export default class PricesList extends Component {
     constructor(props){
         super(props);
 
-        this.state = { prices: [] };
+        this.state = { 
+                        prices: [] ,
+                        prices1: [],
+                        prices2: [],
+                        prices3: [],
+                        prices4: [],
+                        prices5: [],
+                        prices6: [],
+                        prices7: [],
+                        prices8: [],
+                        prices9: [],
+                        prices10: []
+                     };
     }
 
     componentDidMount()
@@ -27,7 +40,19 @@ export default class PricesList extends Component {
         setInterval( () => {
             axios.get('http://localhost:5000/crypto')
             .then(response => {
-                this.setState({ prices: response.data})
+                this.setState({ 
+                    prices: response.data,
+                    prices1: response.data.slice(0,9),
+                    prices2: response.data.slice(10,19),
+                    prices3: response.data.slice(20,29),
+                    prices4: response.data.slice(30,39),
+                    prices5: response.data.slice(40,49),
+                    prices6: response.data.slice(50,59),
+                    prices7: response.data.slice(60,69),
+                    prices8: response.data.slice(70,79),
+                    prices9: response.data.slice(80,89),
+                    prices10: response.data.slice(90,99)
+                })
             })
             .catch((error) => {
                 console.log(error);
@@ -38,11 +63,36 @@ export default class PricesList extends Component {
     render() {
         var outputData = new Array(10);
 
-        for(var i = 0; i < 10; i++)
-        {
-            outputData[i] = this.state.prices.slice(i * 10, i + 9).map(
-                (currentprice) =>  <Price price = {currentprice} />)
-        }
+        outputData[0] = this.state.prices1.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[1] = this.state.prices2.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[2] = this.state.prices3.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[3] = this.state.prices4.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+        
+        outputData[4] = this.state.prices5.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[5] = this.state.prices6.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[6] = this.state.prices7.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[7] = this.state.prices8.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+
+        outputData[8] = this.state.prices9.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+        
+        outputData[9] = this.state.prices10.map(
+            (currentprice) =>  <Price price = {currentprice} />)
+        
 
         for(var i = 0; i < 10; i++)
         {
@@ -71,19 +121,20 @@ export default class PricesList extends Component {
             <CarouselProvider
             naturalSlideWidth={100}
             naturalSlideHeight={125}
+            isPlaying = {true}
             totalSlides={10}
           >
                             <Slider>
-                            <Slide index={0}><div className="my-slide primary">{outputData[0]}</div></Slide>
-                            <Slide index={1}><div className="my-slide primary">{outputData[1]}</div></Slide>
-                            <Slide index={2}><div className="my-slide primary">{outputData[2]}</div></Slide>
-                            <Slide index={3}><div className="my-slide primary">{outputData[3]}</div></Slide>
-                            <Slide index={4}><div className="my-slide primary">{outputData[4]}</div></Slide>
-                            <Slide index={5}><div className="my-slide primary">{outputData[5]}</div></Slide>
-                            <Slide index={6}><div className="my-slide primary">{outputData[6]}</div></Slide>
-                            <Slide index={7}><div className="my-slide primary">{outputData[7]}</div></Slide>
-                            <Slide index={8}><div className="my-slide primary">{outputData[8]}</div></Slide>
-                            <Slide index={9}><div className="my-slide primary">{outputData[9]}</div></Slide>
+                            <Slide index={0}>{outputData[0]}</Slide>
+                            <Slide index={1}>{outputData[1]}</Slide>
+                            <Slide index={2}>{outputData[2]}</Slide>
+                            <Slide index={3}>{outputData[3]}</Slide>
+                            <Slide index={4}>{outputData[4]}</Slide>
+                            <Slide index={5}>{outputData[5]}</Slide>
+                            <Slide index={6}>{outputData[6]}</Slide>
+                            <Slide index={7}>{outputData[7]}</Slide>
+                            <Slide index={8}>{outputData[8]}</Slide>
+                            <Slide index={9}>{outputData[9]}</Slide>
                             </Slider>
             </CarouselProvider>
                         
