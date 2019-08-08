54fa8367a172b9c86bc710a4f3043590c6767b73
randyajackson <rajackson.dev@gmail.com>
2019-08-08 17:01:08 -0600

    8/8/19 5p

--- scrapers/newMovies.js
+++ scrapers/newMovies.js
@@ -1,12 +1,21 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 const url = 'https://www.rottentomatoes.com/api/private/v2.0/browse?type="opening"';
 
+var title;
+var releaseDate;
+var rating;
+var synopsis;
+var posters;
+
 axios.get(url)
 .then(response => {
-    //const $ = cheerio.load(response.data)
-    console.log(  response.data );
+    console.log(  response.data["counts"]["total"] );
+    for(var i = 0; i < response.data["counts"]["total"]; i++)
+    {
+        title = response.data["results"][i]["title"];
+    } 
+    console.log(title);
 })
 .catch(error => {
     console.log(error);
