f3a035282e44216b7dde244800dbc53d7db0c50b
randyajackson <rajackson.dev@gmail.com>
2019-08-09 16:29:49 -0600

    8/9/19 4:30p

--- scrapers/newOnSteam.js
+++ scrapers/newOnSteam.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const allURL = "http://api.steampowered.com/ISteamApps/GetAppList/v2/";
 const singleURL = "https://store.steampowered.com/api/appdetails?appids=";
 
+var fullList = [];
+//--
 var appid;
 var queryURL;
 //--
@@ -16,23 +18,27 @@ var name;
 var release_date;
 var short_description;
 var header_image;
+//--
+var dataCollect = new Object();
+
+dataCollect.getAll = function (){
+    var promise = axios.get(allURL)
+    .then(response => { return response; } )
+    .catch(error => { console.log(error)});
+
+    return promise;
+}
+
+dataCollect.getAll().then(function(result){
+    for(var i = 0; i < result.data["applist"]["apps"].length; i++)
+    {
+        appid = result.data["applist"]["apps"][i]["appid"];
+        fullList.push(appid);
+    } 
+    console.log(fullList);
+});
 
 
 
-axios.get(allURL)
-    .then(response => {
-        for(var i = 0; i < response.data["applist"]["apps"].length; i++)
-        {
-            console.log("in loop one");
-            appid = response.data["applist"]["apps"][i]["appid"];
 
-            queryURL = singleURL + appid;
-            
-            axios.get(queryURL)
-            .then(response2 => {
-                //if(response2.data[appid]["success"] === true)
-                    console.log(response2.data[appid]["success"]);
-            })
 
-        }
-    })
