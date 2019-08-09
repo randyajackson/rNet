233e23892c4ea9130bf3b9dddba7b1c11e06cb99
randyajackson <rajackson.dev@gmail.com>
2019-08-09 12:21:07 -0600

    8/9/19 12:21p

--- scrapers/newMovies.js
+++ scrapers/newMovies.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const mongoose = require('mongoose');
 
 const url = 'https://www.rottentomatoes.com/api/private/v2.0/browse?type="opening"';
 
@@ -6,18 +7,64 @@ var title;
 var releaseDate;
 var rating;
 var synopsis;
-var posters;
+var poster;
 
-axios.get(url)
-.then(response => {
-    console.log(  response.data["counts"]["total"] );
-    for(var i = 0; i < response.data["counts"]["total"]; i++)
-    {
-        title = response.data["results"][i]["title"];
-    } 
-    console.log(title);
-})
-.catch(error => {
-    console.log(error);
-})
+var upcomingMovie;
+
+mongoose.connect('mongodb://localhost/upcoming_movies', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+
+db.once('open', function() {
+
+    db.dropCollection('upcoming_movies', function(err, result){
+        if(err) return console.error(err);
+        console.log("dropped collection");    
+    });
+
+    var upcomingSchema = mongoose.Schema({
+        title: String,
+        releaseDate: String,
+        rating: String,
+        synopsis: String,
+        poster: String
+    });
+
+    var upcomingModel = mongoose.model('upcoming_movie', upcomingSchema);
+    
+    axios.get(url)
+    .then(response => {
+        console.log(  response.data["counts"]["total"] );
+        for(var i = 0; i < parseInt(response.data["counts"]["total"]); i++)
+        {
+            title = response.data["results"][i]["title"];
+            releaseDate = response.data["results"][i]["theaterReleaseDate"];
+            rating = response.data["results"][i]["mpaaRating"];
+            synopsis = response.data["results"][i]["synopsis"];
+            poster = response.data["results"][i]["posters"]["primary"];
+            
+            upcomingMovie = new upcomingModel({
+                title: title,
+                releaseDate: releaseDate,
+                rating: rating,
+                synopsis: synopsis,
+                poster: poster    
+            });
+
+            upcomingMovie.save(function (err, upcoming) {
+                if(err) return console.error(err);
+                console.log("saved:" + upcoming.title)
+            });
+        } 
+
+    })
+    .catch(error => {
+        console.log(error);
+    })
+});
+
+
+
 
--- scrapers/topSearches.py
+++ scrapers/topSearches.py
@@ -1,6 +1,13 @@
+from pymongo import MongoClient
+import pymongo
 from pytrends.request import TrendReq
 
 pytrend = TrendReq()
 
 interest_over_time_df = pytrend.trending_searches(pn='united_states')
-print(interest_over_time_df)
+returnData = interest_over_time_df.to_dict()
+
+for x in returnData[0]:
+    place = x + 1;
+    topic = returnData[0][x];
+    print(place, ' ', topic)
