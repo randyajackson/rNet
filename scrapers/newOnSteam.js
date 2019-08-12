571472537b7057006d7b154265d265688adf826e
randyajackson <rajackson.dev@gmail.com>
2019-08-12 11:08:39 -0600

    8/12/19 11:10p

--- scrapers/newOnSteam.js
+++ scrapers/newOnSteam.js
@@ -3,10 +3,14 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const jq = require('node-jq');
 
+
 const allURL = "http://api.steampowered.com/ISteamApps/GetAppList/v2/";
 const singleURL = "https://store.steampowered.com/api/appdetails?appids=";
 
 var fullList = [];
+var detailList = [];
+var queryResult = {};
+
 //--
 var appid;
 var queryURL;
@@ -19,26 +23,63 @@ var name;
 var release_date;
 var short_description;
 var header_image;
+var price;
+var genres;
 //--
-var dataCollect = new Object();
+dataCollect();
 
-dataCollect.getAll = function (){
-    var promise = axios.get(allURL)
-    .then(response => { return response; } )
+function dataCollect(){
+    axios.get(allURL)
+    .then(result => getIDArray(result))
+    .then(idArray => getDetails(idArray))
     .catch(error => { console.log(error)});
-
-    return promise;
 }
 
-dataCollect.getAll().then(function(result){
+function getIDArray(result){
     for(var i = 0; i < result.data["applist"]["apps"].length; i++)
     {
         appid = result.data["applist"]["apps"][i]["appid"];
         fullList.push(appid);
     } 
-    console.log(fullList);
-});
+    return fullList;
+}
 
+function getDetails(idArray){
+    for(var i = 0; i < idArray.length; i++)
+    {
+        detailList = processQuery(idArray[i], detailList);    
+    } 
+    
+    console.log(detailList);
+
+    return detailList;
+}
+
+function processQuery(id, descriptionArray){
+    axios.get(singleURL + id)
+        .then(fullDetailQuery => {
+
+            if(fullDetailQuery[id]["success"] == true
+            && fullDetailQuery[id]["data"]["type"] == "game"
+            && fullDetailQuery[id]["data"]["release_date"]["coming_soon"] == false)
+
+                queryResult["id"] = id;
+                queryResult["name"] = fullDetailQuery[id]["data"]["name"];
+                queryResult["release_date"] = fullDetailQuery[id]["data"]["release_date"]["date"];
+                queryResult["short_description"] = fullDetailQuery[id]["data"]["short_description"];
+                queryResult["header_image"] = fullDetailQuery[id]["data"]["header_image"];
+                queryResult["price"] = fullDetailQuery[id]["data"]["price_overview"]["final_formatted"];
+                queryResult["genres"] = fullDetailQuery[id]["data"]["genres"];
+
+                descriptionArray.push(queryResult);
+
+                queryResult = {};
+            
+            return descriptionArray;
+
+        })
+        .catch(error => { console.log(error) });
+}
 
 
 
