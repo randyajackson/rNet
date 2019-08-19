a95262aac42c070aa1784003842a643d43ae0dd7
randyajackson <rajackson.dev@gmail.com>
2019-08-19 15:11:11 -0600

    8/19/19 3:15p

--- scrapers/newOnSteam.js
+++ scrapers/newOnSteam.js
@@ -199,7 +199,7 @@ async function narrowArray(records, ids){
                     queryResult["price"] = "Free";
                 else
                 {
-                    if(records[i].data[ ids[i] ]["data"]["price_overview"]["final_formatted"])
+                    if(records[i].data[ ids[i] ]["data"]["price_overview"]["final_formatted"] !== "undefined")
                         queryResult["price"] = records[i].data[ ids[i] ]["data"]["price_overview"]["final_formatted"];
                     else
                         queryResult["price"] = "Price not set";
