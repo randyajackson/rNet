7506716e2d151131c2c5b1881f77cbfdd368d881
randyajackson <rajackson.dev@gmail.com>
2019-08-26 20:21:34 -0600

    8/26/19 8:20p

--- scrapers/bandcamp.js
+++ scrapers/bandcamp.js
@@ -1,7 +1,5 @@
 const axios = require('axios')
 
-
-
 function fetchSales() 
 {
     axios.get('https://bandcamp.com/api/salesfeed/1/get?start_date=1566350340')
@@ -11,6 +9,20 @@ function fetchSales()
 
 fetchSales();
 
-
-
 module.exports = fetchSales;
+
+//getting the data
+
+//1
+//get initial - https://bandcamp.com/api/salesfeed/1/get_initial
+//parse the results
+//wait 120 seconds
+
+
+//2
+//get next batch -https://bandcamp.com/api/salesfeed/1/get?start_date=(end date from get_initial + 60)
+//parse the results
+//wait 120 seconds
+
+//tracking the sales numbers
+
