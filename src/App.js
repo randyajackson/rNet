d09c7bec7fbd880bc3a5866c18ce8e554c5c4224
randyajackson <rajackson.dev@gmail.com>
2019-09-06 15:26:07 -0600

    9/6/19 3:30p

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import '../src/css/Crypto_Component.css';
 import PricesList from "./components/crypto.component";
 import newMovieList from "./components/newMovies.component";
+import BandcampResults from "./components/bandcamp.component";
 
 function App() {
   return (
     <Router>
         <Route path="/crypto" component={PricesList} />
         <Route path="/new_movies" component={newMovieList} />
+        <Route path="/bc" component={BandcampResults} />
     </Router>
   );
 }
---
+++ src/components/bandcamp.component.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import '../css/Bandcamp_Component.css';
+import background from '../videos/test.mp4';
+
+const requester = require('graphql-request');
+
+const bandcampQuery = 
+"{" +
+    "bandcamp" +
+    "{" +
+      "url," +
+      "art_url," +
+      "album_title," +
+      "artist_name," +
+      "item_description," +
+      "count" +
+    "}" +
+"}";
+
+const BC_Results = props => (
+    <div>
+        <div class = "single_result">
+            <div class = "album_art">
+                <img src = {props.results.art_url} alt = {props.results.art_url}></img>
+            </div>
+            <div class = "artist_name">{props.results.artist_name}</div>
+            <div class = "album_title">{props.results.album_title}</div>
+            <div class = "item_description">{props.results.item_description}</div>
+            <div class = "url">http:{props.results.url}</div>
+            <div class = "count"><h1>{props.results.count} sold</h1></div>
+
+        </div>
+    </div>
+)
+
+export default class BandcampResults extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { 
+                        bandcamp_results: [],
+                        previous_bandcamp_results: []
+                     };
+    }
+
+    componentDidMount()
+    {
+        setInterval( () => {
+
+            requester.request('http://localhost:8000/graphql', bandcampQuery)
+            .then(response => {
+                this.getPrevResult();
+                this.setState({
+                    bandcamp_results: response.bandcamp
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        }, 5000)
+        
+    }
+
+    getPrevResult()
+    {
+        this.setState({
+            previous_bandcamp_results : this.state.bandcamp_results.slice()
+        });
+    }
+
+    render() {   
+        const names = this.state.bandcamp_results.map( (currentResult, index) =>  currentResult.artist_name);
+        const count = this.state.bandcamp_results.map( (currentResult, index) =>  currentResult.count);
+
+        var allProps = this.state.bandcamp_results.map(
+            (currentResult, index) =>  <BC_Results results = {currentResult} prevResults = {this.state.previous_bandcamp_results[index]} index = {index} />);
+        
+        return (
+            <React.Fragment>
+                {/* <video muted loop autoPlay id="bgVideo">
+                    <source src= {background} type="video/mp4" />
+                </video> */}
+                <div class = "all_results">
+                    {allProps}
+                </div>
+            </React.Fragment>
+                        
+        );
+            
+        }
+
+
+    }
