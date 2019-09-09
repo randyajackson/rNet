2812eeded253544777e7096014b92b11f19923b8
randyajackson <rajackson.dev@gmail.com>
2019-09-09 12:54:09 -0600

    9/9/19 12:55p

---
+++ scrapers/sneakers.js
@@ -0,0 +1,156 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const mongoose = require('mongoose');
+
+var upcoming_sneaker;
+
+
+mongoose.connect('mongodb://localhost/upcoming_sneakers', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+
+db.once('open', function() {
+
+    db.dropCollection('upcoming_sneakers', function(err, result){
+        if(err) return console.error(err);
+        console.log("dropped collection");    
+    });
+
+    var upcomingSchema = mongoose.Schema({
+        title: String,
+        style: String,
+        thumbnail: String,
+        day: String,
+        month: String
+    });
+
+    var upcomingModel = mongoose.model('upcoming_sneaker', upcomingSchema);
+
+
+    const url = 'https://www.kixify.com/release-dates/upcoming?page=1';
+
+    axios.get(url)
+    .then(response => {
+        var dates = [];
+
+        const $ = cheerio.load(response.data)
+
+        //adding release month and day to dictionary
+        $('.event-date','.release-date-item-continer')
+        .each(function(i, elem) {
+            dates.push(processText($(this).text()));
+        }); 
+        
+        //adding image thumbnail url to dictionary
+        $('.img-responsive', '.thumbnail')
+        .each(function(i, elem) {
+            dates[i].thumbnail = $(this).attr("src") ;
+        });
+
+        //adding title of shoe
+        $('.release-date-title')
+        .each(function(i, elem) {
+            dates[i].title = $(this).text() ;
+        });
+
+        //adding style of shoe
+        $('.release-date-style')
+        .each(function(i, elem) {
+            dates[i].style = $(this).text() ;
+        });
+
+        for(var i = 0; i < dates.length; i++)
+        {
+            upcoming_sneaker = new upcomingModel({
+                title: dates[i].title,
+                style: dates[i].style,
+                thumbnail: dates[i].thumbnail,
+                day: dates[i].day,
+                month: dates[i].month   
+            });
+
+            upcoming_sneaker.save(function (err, upcoming) {
+                if(err) return console.error(err);
+                console.log("saved:" + upcoming.title)
+            });
+        }
+                
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+});
+
+    function processText(inputText) {
+        var output = [];
+        var json = inputText.split(' ');
+
+        json.forEach(function (item) {
+            output.push(item.replace(/\'/g, '').split(/(\d+)/).filter(Boolean));
+        });
+
+        var monthName;
+
+        //switch case for month names
+        switch(output[0][1]){
+            case "Jan": 
+            monthName = "January";
+            break;
+
+            case "Feb": 
+            monthName = "February";
+            break;
+
+            case "Mar": 
+            monthName = "March";
+            break;
+
+            case "Apr": 
+            monthName = "April";
+            break;
+
+            case "May": 
+            monthName = "May";
+            break;
+
+            case "Jun": 
+            monthName = "June";
+            break;
+
+            case "Jul": 
+            monthName = "July";
+            break;
+
+            case "Aug": 
+            monthName = "August";
+            break;
+
+            case "Sep": 
+            monthName = "September";
+            break;
+
+            case "Oct": 
+            monthName = "October";
+            break;
+
+            case "Nov": 
+            monthName = "November";
+            break;
+
+            case "Dec": 
+            monthName = "December";
+            break;
+
+            default:
+            monthName = output[0][1];
+        }
+
+        var returnVariables = {'day' : output[0][0], 'month' : monthName};
+
+        return returnVariables;
+    }
+
+
