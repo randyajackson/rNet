eb1ace4f764acda9bd9ebe6f31d1e7c0a91b244b
randyajackson <rajackson.dev@gmail.com>
2019-09-20 10:51:45 -0600

    9/20/19 10:50a

--- scrapers/crypto.py
+++ scrapers/crypto.py
@@ -1,10 +1,13 @@
 import time
+import sys
 from urllib.request import urlopen as uReq
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pymongo import MongoClient
 import pymongo
 import threading
+import datetime
+from datetime import datetime
 
 chrome_path = r"C:\Users\dell\Desktop\chromeDriver\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
@@ -29,44 +32,55 @@ def collect():
 
 
     client = MongoClient()
+
     db = client['crypto']
     prices = db.crypto_data_prices
 
+    db2 = client['cryptoDebug']
+    debug = db2.crypto_debug
+
     x = 1
     #print(data)
-    if "crypto_data_prices" in db.list_collection_names():
-        while x < len(data): 
-            try:  
-                prices.update_one(
-                    {"coinName" : data[x + 1] },
-                        {"$set" :
-                            {
-                                'id#' : data[x],
-                                'coinName' : data[x + 1],
-                                'coinSName' : data[x + 2],
-                                'coinPrice' : data[x + 3],
-                                'coinTotal' : data[x + 4],
-                                'coin24' : data[x + 6]
+    try: 
+        if "crypto_data_prices" in db.list_collection_names():
+            while x < len(data):  
+                    prices.update_one(
+                        {"coinName" : data[x + 1] },
+                            {"$set" :
+                                {
+                                    'id#' : data[x],
+                                    'coinName' : data[x + 1],
+                                    'coinSName' : data[x + 2],
+                                    'coinPrice' : data[x + 3],
+                                    'coinTotal' : data[x + 4],
+                                    'coin24' : data[x + 6]
+                                }
                             }
-                        }
+                    )
+                    x += 7
+        else:
+            while x < len(data):
+                prices.insert_one(
+                    {
+                        'id#' : data[x],
+                        'coinName' : data[x + 1],
+                        'coinSName' : data[x + 2],
+                        'coinPrice' : data[x + 3],
+                        'coinTotal' : data[x + 4],
+                        'coin24' : data[x + 5]        
+                    }
                 )
                 x += 7
-            except:
-                print("error")
+    except:
+            e = sys.exc_info()[0]
 
-    else:
-        while x < len(data):
-            prices.insert_one(
+            debug.insert_one(
                 {
-                    'id#' : data[x],
-                    'coinName' : data[x + 1],
-                    'coinSName' : data[x + 2],
-                    'coinPrice' : data[x + 3],
-                    'coinTotal' : data[x + 4],
-                    'coin24' : data[x + 5]        
+                    'dateOfIssue' : "{:%B %d, %Y}".format(datetime.now()),
+                    'error' : 'Error with updating database: %s' % e
                 }
             )
-            x += 7
+            print("error")
 
     driver.refresh()
     time.sleep(5)
