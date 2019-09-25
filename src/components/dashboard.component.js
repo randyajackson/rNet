94a54eaa2e35e61daff154250729fbd046c9c9fc
randyajackson <rajackson.dev@gmail.com>
2019-09-25 15:00:48 -0600

    9/25/19 3p

--- src/components/dashboard.component.js
+++ src/components/dashboard.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import '../css/Dashboard_Component.css';
-import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
+
+const mongoose = require('mongoose');
 
 const requester = require('graphql-request');
 
@@ -23,17 +25,56 @@ function createQuery(dataName){
 
 }
 
-const DebugHeaderProp = props => (
+function getTextAreaText(results){
+
+    let text = "";
+
+    if(results.length > 0)
+    {
+        for(let i = 0; i < results.length; i++)
+        {
+            text += results[i].dateOfIssue + ": " + results[i].error + "\n";
+        }
+    }
+
+    return text;
+
+}
+
+function dropDatabase(dbName, dbCollection){
+
+    console.log(dbName + " " + dbCollection);
+
+    
+    mongoose.connect('mongodb://localhost/upcoming_sneakers', {useNewUrlParser: true});
+    var db = mongoose.connection;
+    db.once('open', function() {
+        db.dropCollection('newsneakers', function(err, result){
+            if(err) return console.error(err);
+            console.log("dropped collection");    
+        });
+    });
+}
+
+const DebugConsoleProp = props => (
         <div className = "single_debug_console">
-            <div className = "header"> <b>{props.header}</b> </div>
-            <div className = "status"> Status: dot </div>
-            <div className = "dropDB"> X </div>
+            <div className = "header"><b>{props.header}</b></div>
+
+            {(() => {
+                if(props.errorText === "")
+                    return(<div className = "status" id = "green" ><span role="img" aria-label="check">Status: ✔</span></div>);
+                else
+                    return(<div className = "status" id = "red"><span role="img" aria-label="x">Status: ❌</span></div>);
+            })()}    
+
+            <div className = "dropDB"><Link to="#" onClick={() => dropDatabase(props.dbName, props.collection)}>Clear</Link></div>
+
+            <div className = "textArea">
+                <textarea readOnly value = {props.errorText} cols = "70" rows = "5"  />
+            </div>
+
         </div>
 )
-// {props.results.name}
-//             {props.results.dateOfIssue}
-//             {props.results.error}
-
 
 export default class DebugDashboard extends Component {
 
@@ -92,30 +133,47 @@ export default class DebugDashboard extends Component {
         
         var headerNames = ["Bandcamp", "New Movies", "New Sneakers", "Crypto", "Top Searches"];
 
-        var bandcampDebugHeader = <DebugHeaderProp header = {headerNames[0]} /> ;
+        var dbNames = ["bandcampDebug", "upcoming_movies_debug", "upcoming_sneakers_debug", 
+                       "cryptoDebug", "googleSearchesDebug"];
 
-        // var bandcampDebug = this.state.bandcamp_debug.map(
-        //     (currentResult, header) =>  <DebugProp results = {currentResult} header = {headerNames[0]} />);
+        var collectionNames = ["bandcamp_debugs", "upcoming_movies_debugs", "sneakers_debugs",
+                               "crypto_debug", "top_searches_debug"];
+
+        var bandcampDebugText = getTextAreaText(this.state.bandcamp_debug); 
+        var bandcampDebugHeader = <DebugConsoleProp 
+                                    header = {headerNames[0]} 
+                                    errorText = {bandcampDebugText} 
+                                    dbName = {dbNames[0]} 
+                                    collection = {collectionNames[0]} /> ;
         
-        var newMoviesDebugHeader = <DebugHeaderProp header = {headerNames[1]} /> ;
-
-        // var newMoviesDebug = this.state.new_movie_debug.map(
-        //     (currentResult, header) =>  <DebugProp results = {currentResult} header = {headerNames[1]} />);
+        var newMoviesDebugText = getTextAreaText(this.state.new_movie_debug);
+        var newMoviesDebugHeader = <DebugConsoleProp 
+                                    header = {headerNames[1]} 
+                                    errorText = {newMoviesDebugText} 
+                                    dbName = {dbNames[1]} 
+                                    collection = {collectionNames[1]}/> ;
         
-        var upcomingSneakersDebugHeader = <DebugHeaderProp header = {headerNames[2]} /> ;
+        var upcomingSneakersDebugText = getTextAreaText(this.state.upcoming_sneakers_debug);
+        var upcomingSneakersDebugHeader = <DebugConsoleProp 
+                                            header = {headerNames[2]} 
+                                            errorText = {upcomingSneakersDebugText} 
+                                            dbName = {dbNames[2]} 
+                                            collection = {collectionNames[2]}/> ;
 
-        // var upcomingSneakersDebug = this.state.upcoming_sneakers_debug.map(
-        //     (currentResult, header) =>  <DebugProp results = {currentResult} header = {headerNames[2]} />);
-        
-        var cryptoDebugHeader = <DebugHeaderProp header = {headerNames[3]} /> ;
+        var cryptoDebugText = getTextAreaText(this.state.crypto_debug);
+        var cryptoDebugHeader = <DebugConsoleProp 
+                                    header = {headerNames[3]} 
+                                    errorText = {cryptoDebugText} 
+                                    dbName = {dbNames[3]} 
+                                    collection = {collectionNames[3]}/> ;
 
-        // var cryptoDebug = this.state.crypto_debug.map(
-        //     (currentResult, header) =>  <DebugProp results = {currentResult} header = {headerNames[3]} />);
+        var topSearchesDebugText = getTextAreaText(this.state.top_searches_debug);
+        var topSearchesDebugHeader = <DebugConsoleProp 
+                                        header = {headerNames[4]} 
+                                        errorText = {topSearchesDebugText} 
+                                        dbName = {dbNames[4]} 
+                                        collection = {collectionNames[4]}/> ;
 
-        var topSearchesDebugHeader = <DebugHeaderProp header = {headerNames[4]} /> ;
-
-        // var topSearchesDebug = this.state.top_searches_debug.map(
-        //     (currentResult, header) =>  <DebugProp results = {currentResult} header = {headerNames[4]} />);
 
         return (
             <React.Fragment>
